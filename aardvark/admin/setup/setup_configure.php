f90f4b97d9f8ce06cac64d8c3d3cc4169f0aaea6
Brice Burgess <490144+briceburg@users.noreply.github.com>
2006-05-06 02:17:47 +0000

    /admin/setup/* brought updated with new framework.

--- aardvark/admin/setup/setup_configure.php
+++ aardvark/admin/setup/setup_configure.php
@@ -12,295 +12,110 @@
  * 
  ** [END HEADER]**/
 
-// TODO, make demo_mode a radio on/off . Validate further advanced settings.. ie. mailNum can't exceed MailMax, etc. They should also be divisible...'
-
+/**********************************
+	INITIALIZATION METHODS
+ *********************************/
 define('_IS_VALID', TRUE);
 
-require('../../bootstrap.php');
-require_once (bm_baseDir.'/inc/db_procedures.php');
-require_once (bm_baseDir.'/inc/class.bform.php');
-$poMMo = & fireup("secure");
+require ('../../bootstrap.php');
+require_once (bm_baseDir . '/inc/db_procedures.php');
+
+$poMMo = & fireup('secure');
+$logger = & $poMMo->logger;
 $dbo = & $poMMo->openDB();
-$form = new bForm();
 
-$updated = FALSE;
-// ## FORM: CHECK FORM INPUT
-if ($form->validateForm()) {
-	// ## FORM: VALID INPUT. UPDATE CONFIG
+/**********************************
+	SETUP TEMPLATE, PAGE
+ *********************************/
+$smarty = & bmSmartyInit();
+$smarty->prepareForForm();
 
-	// convert password to MD5 if given...
-	if (!empty ($form->_input['admin_password']))
-		$form->_input['admin_password'] = md5($form->_input['admin_password']);
+if (!SmartyValidate :: is_registered_form() || empty ($_POST)) {
+	// ___ USER HAS NOT SENT FORM ___
 
-	/*/ convert demo checkbox if off 
-	if (empty($form->_input['demo_mode']))
-		$form->_input['demo_mode'] = 'off'; */
+	SmartyValidate :: connect($smarty, true);
 
-	dbUpdateConfig($dbo, $form->_input);
 
-	$poMMo->loadConfig();
-	$updated = TRUE;
-	$form->inputClear();
+	SmartyValidate :: register_validator('admin_username', 'admin_username', 'notEmpty', false, false, 'trim');
+	SmartyValidate :: register_validator('site_name', 'site_name', 'notEmpty', false, false, 'trim');
+	SmartyValidate :: register_validator('list_name', 'list_name', 'notEmpty', false, false, 'trim');
+	SmartyValidate :: register_validator('list_fromname', 'list_fromname', 'notEmpty', false, false, 'trim');
+
+	SmartyValidate :: register_validator('site_url', 'site_url', 'isURL', false, false, 'trim');
+	SmartyValidate :: register_validator('site_success', 'site_success', 'isURL', TRUE);
+	SmartyValidate :: register_validator('site_confirm', 'site_confirm', 'isURL', TRUE);
+
+	SmartyValidate :: register_validator('admin_password2', 'admin_password:admin_password2', 'isEqual',TRUE);
+
+	SmartyValidate :: register_validator('admin_email', 'admin_email', 'isEmail');
+	SmartyValidate :: register_validator('list_fromemail', 'list_fromemail', 'isEmail');
+	SmartyValidate :: register_validator('list_frombounce', 'list_frombounce', 'isEmail');
+
+
+	$formError = array ();
+	$formError['admin_username'] = $formError['sitename'] = 
+	$formError['list_name'] = $formError['list_fromname'] = _T('Cannot be empty.');
+
+	$formError['admin_email'] = $formError['list_fromemail'] = 
+	$formError['list_frombounce'] = _T('Invalid email address');
+
+	$formError['admin_password2'] = _T('Passwords must match.');
+
+	$formError['site_url'] = $formError['site_success'] = 
+	$formError['site_confirm'] = _T('Must be a valid URL');
+
+	$smarty->assign('formError', $formError);
+
+	// populate _POST with info from database (fills in form values...) 
+	$dbVals = $poMMo->getConfig(array (
+		'admin_username',
+		'site_success',
+		'site_confirm',
+		'list_fromname',
+		'list_fromemail',
+		'list_frombounce',
+		'list_exchanger',
+		'list_confirm'
+	));
+
+	$dbVals['demo_mode'] = (!empty ($poMMo->_config['demo_mode']) && ($poMMo->_config['demo_mode'] == "on")) ? 'on' : 'off';
+
+	$dbVals['site_url'] = $poMMo->_config['site_url'];
+	$dbVals['site_name'] = $poMMo->_config['site_name'];
+	$dbVals['admin_email'] = $poMMo->_config['admin_email'];
+	$dbVals['list_name'] = $poMMo->_config['list_name'];
+
+	$smarty->assign($dbVals);
+} else {
+	// ___ USER HAS SENT FORM ___
+	SmartyValidate :: connect($smarty);
+
+	if (SmartyValidate :: is_valid($_POST)) {
+		// __ FORM IS VALID
+
+		// convert password to MD5 if given...
+		if (!empty ($_POST['admin_password']))
+			$_POST['admin_password'] = md5($_POST['admin_password']);
+
+		$oldDemo = $poMMo->_config['demo_mode'];
+		
+		dbUpdateConfig($dbo, $_POST);
+
+		$poMMo->loadConfig();
+		
+		$logger->addMsg(_T('Configuration Updated.'));
+		
+		// refresh page to reflect demonstration mode changes
+		if ($oldDemo != $poMMo->_config['demo_mode'])
+			bmRedirect('setup_configure.php');
+
+	} else {
+		// __ FORM NOT VALID
+		$logger->addMsg(_T('Please review and correct errors with your submission.'));
+	}
 }
 
-// ## FORM: FORM WAS NOT SUBMITTED, OR HAD INVALID INPUT.
-
-/** poMMo templating system **/
-
-// header settings -->
-$_head = "\n<link href=\"".bm_baseUrl."/inc/css/bform.css\" rel=\"stylesheet\" type =\"text/css\">\n<script src=\"".bm_baseUrl."/inc/js/bform.js\" type=\"text/javascript\"></script>";
-
-$_nologo = FALSE;
-$_menu = array ();
-$_menu[] = "<a href=\"".bm_baseUrl."/index.php?logout=TRUE\">Logout</a>";
-$_menu[] = "<a href=\"admin_setup.php\">Setup Page</a>";
-$_menu[] = "<a href=\"".$poMMo->_config['site_url']."\">".$poMMo->_config['site_name']."</a>";
-
-// right bar settings -->
-$_nomenu = FALSE; // turn off main "admin menu" in right bar
-$_nodemo = FALSE; // turn off display of poMMo demonstration mode status
-
-$_extmenu = array ();
-$_extmenu['name'] = "poMMo Setup";
-$_extmenu['links'] = array ();
-$_extmenu['links'][] = "<a href=\"setup_configure.php\">Configure</a>";
-$_extmenu['links'][] = "<a href=\"setup_demographics.php\">Demographics</a>";
-$_extmenu['links'][] = "<a href=\"setup_form.php\">Setup Form</a>";
-
-include (bm_baseDir.'/setup/top.php');
-
-/** End templating system **/
-
-echo "
-<h1>Settings</h1>
-
-<img src=\"".bm_baseUrl."/img/icons/settings.png\" class=\"articleimg\">
-
-<p>
-Use this page to configure poMMo. You can change the login information, set website and mailing list parameters, end enable demonstration mode. If you enable demonstration mode, no emails will be sent from the system.
-</p>
-";
-
-if ($updated)
-	echo "<p><b>Settings Updated!</b></p>";
-
-// setup a shortcut for an often typed string. class="required" references styling in bform.css
-$reqStr = '<span class="required" title="This field is required.">*</span>';
-
-$config = $poMMo->getConfig(array('admin_username','site_success','site_confirm','list_fromname','list_fromemail','list_frombounce','list_exchanger','list_confirm','mailMax','mailNum','mailSize','mailDelay'));
-
-$form->inputLoad($poMMo->dataGet());
-$form->startForm();
-
-// print any errors that occured
-$form->printErrors();
-
-$form->newFieldSet("Administrative");
-
-$form->newField("admin_username");
-$form->setField("prompt", "Administrator Username: ".$reqStr);
-$form->setField("type", "text");
-$form->setField("default", "enter username");
-if (!empty ($config['admin_username']))
-	$form->setField("init", $config['admin_username']);
-$form->setField("notes", "(you will use this to login)");
-$form->setField("misc", "maxlength=\"60\" size=\"32\"");
-$form->setField("validate", "req");
-$form->setField("error", "Invalid Administrator Username");
-$form->printField();
-
-$form->newField("admin_password");
-$form->setField("prompt", "Administrator Password: ");
-$form->setField("type", "text");
-$form->setField("default", "enter new password");
-$form->setField("notes", "(you will use this to login)");
-$form->setField("misc", "maxlength=\"60\" size=\"32\"");
-$form->printField();
-
-$form->newField("admin_password2");
-$form->setField("prompt", "Retype Password: ");
-$form->setField("type", "text");
-$form->setField("default", "enter password again");
-$form->setField("notes", "(enter password again)");
-$form->setField("misc", "maxlength=\"60\" size=\"32\"");
-$form->setField("validate", "opt.same");
-$form->setField("error", "admin_password:Passwords did not match");
-$form->printField();
-
-$form->newField("admin_email");
-$form->setField("prompt", "Administrator Email: ".$reqStr);
-$form->setField("type", "text");
-$form->setField("default", "enter email");
-if (!empty ($poMMo->_config['admin_email']))
-	$form->setField("init", $poMMo->_config['admin_email']);
-$form->setField("notes", "(email address of administrator)");
-$form->setField("misc", "maxlength=\"60\" size=\"32\"");
-$form->setField("validate", "req.email");
-$form->setField("error", "Invalid Administrator Email");
-$form->printField();
-
-$form->endFieldSet();
-
-$form->newFieldSet("Website");
-
-$form->newField("site_name");
-$form->setField("prompt", "Website Name: ".$reqStr);
-$form->setField("type", "text");
-$form->setField("default", "enter website name");
-if (!empty ($poMMo->_config['site_name']))
-	$form->setField("init", $poMMo->_config['site_name']);
-$form->setField("notes", "(The name of your Website)");
-$form->setField("misc", "maxlength=\"60\" size=\"32\"");
-$form->setField("validate", "req");
-$form->setField("error", "Invalid website name");
-$form->printField();
-
-$form->newField("site_url");
-$form->setField("prompt", "Website URL: ".$reqStr);
-$form->setField("type", "text");
-$form->setField("default", "enter website url");
-if (!empty ($poMMo->_config['site_url']))
-	$form->setField("init", $poMMo->_config['site_url']);
-$form->setField("notes", "(Your websites web address)");
-$form->setField("misc", "maxlength=\"60\" size=\"32\"");
-$form->setField("validate", "req.url");
-$form->setField("error", "Invalid website URL");
-$form->printField();
-
-$form->newField("site_success");
-$form->setField("prompt", "Success URL: ");
-$form->setField("type", "text");
-$form->setField("default", "enter success url (optional)");
-if (!empty ($config['site_success']))
-	$form->setField("init", $config['site_success']);
-$form->setField("notes", "(Webpage users will see upon successfull subscription. Leave blank to display default welcome page.)");
-$form->setField("misc", "maxlength=\"60\" size=\"32\"");
-$form->setField("validate", "opt.url");
-$form->setField("error", "Invalid success URL");
-$form->printField();
-
-$form->newField("site_confirm");
-$form->setField("prompt", "Confirm URL: ");
-$form->setField("type", "text");
-$form->setField("default", "enter confirm url (optional)");
-if (!empty ($config['site_confirm']))
-	$form->setField("init", $config['site_confirm']);
-$form->setField("notes", "(Webpage users will see upon subscription attempt. Leave blank to display default confirmation page.)");
-$form->setField("misc", "maxlength=\"60\" size=\"32\"");
-$form->setField("validate", "opt.url");
-$form->setField("error", "Invalid success URL");
-$form->printField();
-
-$form->endFieldSet();
-
-$form->newFieldSet("Mailing List");
-
-$form->newField("demo_mode");
-$form->setField("prompt", "Demonstration Mode: ");
-$form->setField("type", "checkbox");
-$form->setField("notes", "Check this box to enable demonstration mode.");
-if (!empty ($poMMo->_config['demo_mode']) && ($poMMo->_config['demo_mode'] == "on"))
-	$form->setField("default", "checked");
-else
-	$form->setField("default", "unchecked");
-$form->printField();
-
-$form->newField("list_confirm");
-$form->setField("prompt", "Email Confirmation: ");
-$form->setField("type", "checkbox");
-$form->setField("notes", "Check to validate email upon subscription attempt.");
-if (!empty ($config['list_confirm']) && ($config['list_confirm'] == "on"))
-	$form->setField("default", "checked");
-else
-	$form->setField("default", "unchecked");
-//$form->setField("misc", " disabled");
-$form->printField();
-
-$form->newField("list_name");
-$form->setField("prompt", "List Name: ".$reqStr);
-$form->setField("type", "text");
-$form->setField("default", "enter list name");
-if (!empty ($poMMo->_config['list_name']))
-	$form->setField("init", $poMMo->_config['list_name']);
-$form->setField("notes", "(The name of your Mailing List)");
-$form->setField("misc", "maxlength=\"60\" size=\"32\"");
-$form->setField("validate", "req");
-$form->setField("error", "Invalid list name ");
-$form->printField();
-
-$form->newField("list_fromname");
-$form->setField("prompt", "From Name: ");
-$form->setField("type", "text");
-$form->setField("default", "enter from name");
-if (!empty ($config['list_fromname']))
-	$form->setField("init", $config['list_fromname']);
-$form->setField("notes", "(Default name mailings will be sent from)");
-$form->setField("misc", "maxlength=\"60\" size=\"32\"");
-$form->printField();
-
-$form->newField("list_fromemail");
-$form->setField("prompt", "From Email: ");
-$form->setField("type", "text");
-$form->setField("default", "enter from email");
-if (!empty ($config['list_fromemail']))
-	$form->setField("init", $config['list_fromemail']);
-$form->setField("notes", "(Default email mailings will be sent as)");
-$form->setField("misc", "maxlength=\"60\" size=\"32\"");
-$form->setField("validate", "opt.email");
-$form->setField("error", "Invalid from email");
-$form->printField();
-
-$form->newField("list_frombounce");
-$form->setField("prompt", "Bounce Address: ");
-$form->setField("type", "text");
-$form->setField("default", "enter from bounce email");
-if (!empty ($config['list_frombounce']))
-	$form->setField("init", $config['list_frombounce']);
-$form->setField("notes", "(Default email mailings will be sent as)");
-$form->setField("misc", "maxlength=\"60\" size=\"32\"");
-$form->setField("validate", "opt.email");
-$form->setField("error", "Invalid bounce email");
-$form->printField();
-
-$form->endFieldSet();
-
-$form->newFieldSet("Advanced");
-
-$form->newField("list_exchanger");
-$form->setField("prompt", "Mail Exchanger:");
-$form->setField("type", "select");
-if (!empty ($config['list_exchanger']))
-	$form->setField("default", $config['list_exchanger']);
-else
-	$form->setField("default", "sendmail");
-$option = array ();
-$option[] = " :Select mail exchanger"; // note the " :" prefix -- this creates an empty value which will match the required rule if nothing was selected
-$option[] = "sendmail:Sendmail";
-$option[] = "mail:PHP Mail Function (defaults)";
-$option[] = "smtp:SMTP Relay";
-$option[] = "exim:Exim MTA(disabled)";
-
-$form->setField("option", $option); // always prefix with "option" to trigger option event.
-$form->setField("notes", "Select Mail Exchanger");
-//$form->setField("misc", " onChange=\"document.".$form->_name.".submit()\"");
-$form->printField();
-
-if ($config['list_exchanger'] == 'smtp') {
-	echo '<div class="field"><a href="setup_smtp.php"><img src="'.bm_baseUrl.'/img/icons/right.png" align="center" border="0"></a> &nbsp; SMTP Config:';
-	echo ' <a href="setup_smtp.php">Click Here</a> to setup SMTP Relay(s)</div><br /><br />';
-}
-
-echo '<div class="field"><a href="setup_messages.php"><img src="'.bm_baseUrl.'/img/icons/right.png" align="center" border="0"></a> &nbsp; Messages:';
-echo ' <a href="setup_messages.php">Click Here</a> to customize messages.<div class="notes">(define the email message sent for subscription, unsubscription, and updates)</div></div><br />';
-
-echo '<div class="field"><a href="setup_throttle.php"><img src="'.bm_baseUrl.'/img/icons/right.png" align="center" border="0"></a> &nbsp; Throttling:';
-echo ' <a href="setup_throttle.php">Click Here</a> to setup mail throttler.<div class="notes">(controls mails per second, bytes per second, and domain limits)</div></div><br />';
-
-
-$form->endFieldSet();
-
-// print the submit button and close the form element
-$form->endForm();
-
-include (bm_baseDir.'/setup/footer.php');
+$smarty->assign($_POST);
+$smarty->display('admin/setup/setup_configure.tpl');
+bmKill();
 ?>
--- aardvark/admin/setup/setup_smtp.php
+++ aardvark/admin/setup/setup_smtp.php
@@ -1,5 +1,4 @@
 <?php
-
 /** [BEGIN HEADER] **
  * COPYRIGHT: (c) 2005 Brice Burgess / All Rights Reserved    
  * LICENSE: http://www.gnu.org/copyleft.html GNU/GPL 
@@ -12,221 +11,112 @@
  * 
  ** [END HEADER]**/
 
+/**********************************
+	INITIALIZATION METHODS
+ *********************************/
 define('_IS_VALID', TRUE);
 
-require('../../bootstrap.php');
-require_once (bm_baseDir.'/inc/db_procedures.php');
-require_once (bm_baseDir.'/inc/class.bform.php');
-$poMMo = & fireup("secure");
+require ('../../bootstrap.php');
+require_once (bm_baseDir . '/inc/db_procedures.php');
+require_once (bm_baseDir . '/inc/phpmailer/class.phpmailer.php');
+require_once (bm_baseDir . '/inc/phpmailer/class.smtp.php');
+
+$poMMo = & fireup('secure');
+$logger = & $poMMo->logger;
 $dbo = & $poMMo->openDB();
 
+/**********************************
+	SETUP TEMPLATE, PAGE
+ *********************************/
+$smarty = & bmSmartyInit();
+$smarty->prepareForForm();
+$smarty->assign('returnStr', _T('Configure'));
+
 // Read user requested changes	
-if (!empty($_POST['addSmtpServer'])) {
-	$server = array('host' => 'mail.localhost','port' => '25', 'auth' => 'off', 'user' => '', 'pass' => '');
-	$input['smtp_'.key($_POST['addSmtpServer'])] = serialize($server);
-	dbUpdateConfig($dbo,$input,TRUE);
+if (!empty ($_POST['addSmtpServer'])) {
+	$server = array (
+		'host' => 'mail.localhost',
+		'port' => '25',
+		'auth' => 'off',
+		'user' => '',
+		'pass' => ''
+	);
+	$input['smtp_' . key($_POST['addSmtpServer'])] = serialize($server);
+	dbUpdateConfig($dbo, $input, TRUE);
 }
-elseif(!empty($_POST['updateSmtpServer'])) {
+elseif (!empty ($_POST['updateSmtpServer'])) {
 	$key = key($_POST['updateSmtpServer']);
-	$server = array('host' => str2db($_POST['host'][$key]), 'port' => str2db($_POST['port'][$key]), 'auth' => str2db($_POST['auth'][$key]), 'user' => str2db($_POST['user'][$key]), 'pass' => str2db($_POST['pass'][$key]));
-	$input['smtp_'.$key] = serialize($server);
-	dbUpdateConfig($dbo,$input,TRUE);
+	$server = array (
+		'host' => str2db($_POST['host'][$key]
+	), 'port' => str2db($_POST['port'][$key]), 'auth' => str2db($_POST['auth'][$key]), 'user' => str2db($_POST['user'][$key]), 'pass' => str2db($_POST['pass'][$key]));
+	$input['smtp_' . $key] = serialize($server);
+	dbUpdateConfig($dbo, $input, TRUE);
 }
-elseif(!empty($_POST['deleteSmtpServer'])) {
-	$input['smtp_'.key($_POST['deleteSmtpServer'])] = '';
-	dbUpdateConfig($dbo,$input,TRUE);
+elseif (!empty ($_POST['deleteSmtpServer'])) {
+	$input['smtp_' . key($_POST['deleteSmtpServer'])] = '';
+	dbUpdateConfig($dbo, $input, TRUE);
 }
-elseif(!empty($_POST['throttle_smtp'])) {
-	$input['throttle_SMTP'] = str2db($_POST['throttle_smtp']);
-	dbUpdateConfig($dbo,$input);
+elseif (!empty ($_POST['throttle_SMTP'])) {
+	$input['throttle_SMTP'] = str2db($_POST['throttle_SMTP']);
+	dbUpdateConfig($dbo, $input);
 }
 
-/** poMMo templating system **/
-
-// header settings -->
-$_head = "\n<link href=\"".bm_baseUrl."/inc/css/bform.css\" rel=\"stylesheet\" type =\"text/css\">\n<script src=\"".bm_baseUrl."/inc/js/bform.js\" type=\"text/javascript\"></script>";
-
-$_nologo = FALSE;
-$_menu = array ();
-$_menu[] = "<a href=\"".bm_baseUrl."/index.php?logout=TRUE\">Logout</a>";
-$_menu[] = "<a href=\"admin_setup.php\">Setup Page</a>";
-$_menu[] = "<a href=\"".$poMMo->_config['site_url']."\">".$poMMo->_config['site_name']."</a>";
-
-// right bar settings -->
-$_nomenu = FALSE; // turn off main "admin menu" in right bar
-$_nodemo = FALSE; // turn off display of poMMo demonstration mode status
-
-$_extmenu = array ();
-$_extmenu['name'] = "poMMo Setup";
-$_extmenu['links'] = array ();
-$_extmenu['links'][] = "<a href=\"setup_configure.php\">Configure</a>";
-$_extmenu['links'][] = "<a href=\"setup_demographics.php\">Demographics</a>";
-$_extmenu['links'][] = "<a href=\"setup_form.php\">Setup Form</a>";
-
-include (bm_baseDir.'/setup/top.php');
-
-/** End templating system **/
-
-echo "
-<h1>Settings</h1>
-
-<img src=\"".bm_baseUrl."/img/icons/settings.png\" class=\"articleimg\">
-
-<p>
-poMMo can be configured to use up to 4 SMTP servers to relay mail from. Each server can be individually regulated by its own throttle controller, or can share a global one. For maximum output, use individual throttle controllers. Throttling set 
-from the main configuration page will be inherrited.
-</p>
-";
-
-echo '<a href="setup_configure.php"><img src="'.bm_baseUrl.'/img/icons/back.png" align="middle" class="navimage" border=\'0\'>Return to configuration page</a>';
-
-echo '<h2>SMTP Relays &raquo;</h2>';
-
-// used to TEST SMTP connectivity
-require_once (bm_baseDir.'/inc/phpmailer/class.phpmailer.php');
-require_once (bm_baseDir.'/inc/phpmailer/class.smtp.php');
-
-$smtpConfig = $poMMo->getConfig(array ('smtp_1', 'smtp_2', 'smtp_3', 'smtp_4','throttle_SMTP'));
+// Get the SMTP settings from DB
+$smtpConfig = $poMMo->getConfig(array (
+	'smtp_1',
+	'smtp_2',
+	'smtp_3',
+	'smtp_4',
+	'throttle_SMTP'
+));
 
 $smtp[1] = unserialize($smtpConfig['smtp_1']);
 $smtp[2] = unserialize($smtpConfig['smtp_2']);
 $smtp[3] = unserialize($smtpConfig['smtp_3']);
 $smtp[4] = unserialize($smtpConfig['smtp_4']);
 
-if (empty($smtp[1]))
-	$smtp[1] = array('host' => 'mail.localhost','port' => '25', 'auth' => 'off', 'user' => '', 'pass' => '');
+if (empty ($smtp[1]))
+	$smtp[1] = array (
+		'host' => 'mail.localhost',
+		'port' => '25',
+		'auth' => 'off',
+		'user' => '',
+		'pass' => ''
+	);
 
-
-
-$form = new bForm();
-$form->inputLoad($poMMo->dataGet());
-$form->startForm();
-// print any errors that occured
-$form->printErrors();
-
-$form->newFieldSet("SMTP Throttling");
-
-$form->newField("throttle_smtp");
-$form->setField("prompt", "Throttle Controller:");
-$form->setField("type", "select");
-if (!empty ($smtpConfig['throttle_SMTP']))
-	$form->setField("default", $smtpConfig['throttle_SMTP']);
-else
-	$form->setField("default", "individual");
-$option = array ();
-$option[] = "individual:Individual Throttler per Server";
-$option[] = "shared:Share A Global Throttler";
-
-$form->setField("option", $option); // always prefix with "option" to trigger option event.
-$form->setField("notes", "(Throttle Control can be shared or individual)");
-$form->setField("misc", " onChange=\"document.".$form->_name.".submit()\"");
-$form->printField();
-
-$form->endFieldSet();
-	
-
+// Test the servers
 $addServer = FALSE;
-for ($i=1; $i < 5; $i++) {
+$smtpStatus = array ();
+for ($i = 1; $i < 5; $i++) {
 
-if (empty($smtp[$i])) {
-	if (!$addServer)
-		$addServer = $i;
-	continue;	
+	if (empty ($smtp[$i])) {
+		if (!$addServer)
+			$addServer = $i;
+		continue;
+	}
+
+	$test[$i] = new PHPMailer();
+
+	$test[$i]->Host = (empty ($smtp[$i]['host'])) ? null : $smtp[$i]['host'];
+	$test[$i]->Port = (empty ($smtp[$i]['port'])) ? null : $smtp[$i]['port'];
+	if (!empty ($smtp[$i]['auth']) && $smtp[$i]['auth'] == 'on') {
+		$test[$i]->SMTPAuth = TRUE;
+		$test[$i]->Username = (empty ($smtp[$i]['user'])) ? null : $smtp[$i]['user'];
+		$test[$i]->Password = (empty ($smtp[$i]['pass'])) ? null : $smtp[$i]['pass'];
+	}
+	if (@ $test[$i]->SmtpConnect()) {
+		$smtpStatus[$i] = TRUE;
+		$test[$i]->SmtpClose();
+	} else {
+		$smtpStatus[$i] = FALSE;
+	}
 }
 
-$form->newFieldSet("SMTP #".$i."");
-	
-	$test[$i]= new PHPMailer();
-	
-	if (!empty($smtp[$i]['host']))
-		$test[$i]->Host = $smtp[$i]['host']; 
-	if (!empty($smtp[$i]['port']))
-		$test[$i]->Port = $smtp[$i]['port']; 
-	if (!empty($smtp[$i]['auth']) && $smtp[$i]['auth'] == 'on') {
-		$test[$i]->SMTPAuth = TRUE; 
-		if (!empty($smtp[$i]['user']))
-			$test[$i]->Username = $smtp[$i]['user']; 
-		if (!empty($smtp[$i]['pass']))
-			$test[$i]->Password = $smtp[$i]['pass']; 
-	}
-	echo '<div class="field">SMTP Status: <label></label>';
-	if (@$test[$i]->SmtpConnect()) {
-		echo '<img src="'.bm_baseUrl.'/img/icons/ok.png" align="middle"> Connected to SMTP Server.';
-		$test[$i]->SmtpClose();	
-	}
-	else
-		echo '<img src="'.bm_baseUrl.'/img/icons/nok.png" align="middle"> Could not connect to SMTP Server.';
-	echo '</div>';
-	
+$smarty->assign('addServer',$addServer);
+$smarty->assign('smtpStatus',$smtpStatus);
+$smarty->assign('smtp', $smtp);
+$smarty->assign('throttle_SMTP', $smtpConfig['throttle_SMTP']);
 
-	$form->newField('host['.$i.']');
-	$form->setField("prompt", "SMTP Host: ");
-	$form->setField("type", "text");
-	$form->setField("default", "type address of SMTP server");
-	if (!empty ($smtp[$i]['host']))
-		$form->setField("init", $smtp[$i]['host']);
-	$form->setField("notes", "(IP Address or Name of your SMTP server)");
-	$form->setField("misc", "maxlength=\"60\" size=\"32\"");
-	$form->printField();
-
-	$form->newField('port['.$i.']');
-	$form->setField("prompt", "SMTP Port: ");
-	$form->setField("type", "text");
-	$form->setField("default", "Port # of SMTP server");
-	if (!empty ($smtp[$i]['host']))
-		$form->setField("init", $smtp[$i]['port']);
-	else
-		$form->setField("init", '25');
-	$form->setField("notes", "(Port # of SMTP server [usually 25])");
-	$form->setField("misc", "maxlength=\"60\" size=\"32\"");
-	$form->printField();
-
-	$form->newField('auth['.$i.']');
-	$form->setField("prompt", "SMTP Authentication: ");
-	$form->setField("type", "checkbox");
-	$form->setField("notes", "Check this box to enable SMTP Authentication [usually off].");
-	if (!empty ($smtp[$i]['auth']) && ($smtp[$i]['auth'] == "on"))
-		$form->setField("default", "checked");
-	else
-		$form->setField("default", "unchecked");
-	$form->printField();
-
-	$form->newField('user['.$i.']');
-	$form->setField("prompt", "SMTP Username: ");
-	$form->setField("type", "text");
-	$form->setField("default", "type SMTP username");
-	if (!empty ($smtp[$i]['user']))
-		$form->setField("init", $smtp[$i]['user']);
-	$form->setField("notes", "(Username for your SMTP server)");
-	$form->setField("misc", "maxlength=\"60\" size=\"32\"");
-	$form->printField();
-
-	$form->newField('pass['.$i.']');
-	$form->setField("prompt", "SMTP Password: ");
-	$form->setField("type", "text");
-	$form->setField("default", "type SMTP password");
-	if (!empty ($smtp[$i]['pass']))
-		$form->setField("init", $smtp[$i]['pass']);
-	$form->setField("notes", "(Password for your SMTP server)");
-	$form->setField("misc", "maxlength=\"60\" size=\"32\"");
-	$form->printField();
-	
-	echo '<div class="field"><input type="submit" name="updateSmtpServer['.$i.']" id="updateSmtpServer['.$i.']" value="Update Relay #'.$i.'">';
-	if ($i > 1)
-		echo '&nbsp;&nbsp; -- &nbsp;&nbsp;<input type="submit" name="deleteSmtpServer['.$i.']" id="deleteSmtpServer['.$i.']" value="Remove Relay #'.$i.'">';
-	else 
-		echo '&nbsp;&nbsp; -- &nbsp;&nbsp;Relay #1 is your default relay';
-	echo '</div>';
-
-$form->endFieldSet();
-
-}
-
-// print out option to Add another server
-if ($addServer)
-	echo '<br><br><div class="field"><input type="submit" name="addSmtpServer['.$addServer.']" id="addSmtpServer['.$addServer.']" value="Add Another Relay"></div>';
-
-echo '</form>';
-
-include (bm_baseDir.'/setup/footer.php');
+$smarty->display('admin/setup/setup_smtp.tpl');
+bmKill();
 ?>
