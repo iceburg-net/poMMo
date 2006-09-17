71d393f3dad6afbebcc355aa8238187537d4fece
briceb <briceb@00ef1af9-9410-0410-96a5-8f45c99aad9b>
2006-09-17 15:49:22 +0000

    updated TODO

--- aardvark-development/docs/todo.php
+++ aardvark-development/docs/todo.php
@@ -18,56 +18,25 @@ elsewhere
 defined('_IS_VALID') or die('Move along...');
 ?>
 
-
-With template, add predefined support. E.G. ++ADD UNSUBSCRIBE LINK++ SEE; http://www.iceburg.net/pommo/community/viewtopic.php?id=108
-
-
-+++ Implement Passwords on user information (login.php). Include customizable question/answer pair.
-
-+++ Merge subscribers/pending table. ??
-
-+++ Merge current/history mailing table.
-
-(19:40:01) comporder1: on my emails i do not include alternate text... so i turn it off. then i go to mailings_send3 and preview and then need to make a change. when i click edit and it goes back to mailings_send2 it reverts back to alternate text=YES.  Its not a big deal... just one extra click. maybe there is an easy fix?
-
-
-I'd like to add a "comment" to the field types which would output a text area on the subscriber form that could be limited to a certain number of characters, and who's styling would be defined within themes/default/subscribe/form.subscribe.tpl (of course). Perhaps there's a better word for "comment" as a field type?
-
-
-1. when i try to import some subscribers from a CSV file, the script does inject them into the database alright, but the success page doesn't come up (subscribers_import2.php ),
-in firefox it's a blank page and in IE it's a "server not found page". there are 650 subscribers in the csv file if that matters.
-
-2.In the subscribers admin section, the default subscribers number per page is 50. When i try to select a bigger number, it also leads to a blank page.
-
-3. mailings_send.php redirects me to /admin/admin/mailings_send2.php which doesn't exist the same with mailings_send3.php
-
-4. do not forget to put a blank index in all the directories that do not have an index of their own
-
-
-Personally I would also like to see a "chain" in place for the unsubscribe, eg. it calls the unsuscribe as it does now but then continies onto another php file, by default empty. But this would allow users (admins) to implement any further processing that they wanted to do, i would imagine that the persons email address should be "posted" to this php chainer. This could make the integration of this to any other installation of anything else, eg site registration removal, so much easier.
-
-Add validation of subscriber field name (via AJAX?) with personalization -- check on form submit of mailings_send2.php 
-  + Ensure that server side algorithim during mailing_send4.php will not choke on invalid subscriber field name
-  
+[BEFORE 1.0]
+	+ Rewritten Import
+	+ Rewritten Subscriber Manage
+	+ Message Templating
+	+ Rewritten Default Theme (CSS, JS, TEMPLATES)
+		
   
 [BRICE]
-	
-	IMMEDIATE (for next release):
-		+ fix prepareForForm() in SMARTY TEMPLATE (must load proper JS, CSS!)
-			-  test w/ group adding ("type group name" not cleared on focus)
 
-	[BEFORE 1.0]
-		+ Personalization
-		+ Rewritten Import
-		+ Rewritten Subscriber Manage
-		+ Message Templating
-		+
+	IMMEDIATE (FOR NEXT RELEASE):
+		 (feature) Enhanced subscriber management
+		 (feature) Add search capability to subscriber management
+		 (feature) add ability to send messages to list administrator upon successfull subscription/unsubscription/changes
+	
 	
 	SHORT TERM:
 	
 	
 	  (API) - secure "included" files under cache -- don't include them.. rather run them through specialized parser? e.g. for embed.forms & httpSpawn tester
-	  
 	  (API) - override PHPMailers error handling to use logger -- see extending PHPMailer Example @ website
 	  (API) Better mailing send debugging ->
 	    Change queue table to include "status" field --> ie. ENUM ('unsent','sent','failed') + error catching... (including PHP fatal errors) 
@@ -77,44 +46,9 @@ Add validation of subscriber field name (via AJAX?) with personalization -- chec
 	  (API) Allow fetching of field id, names, + types -- NOT OPTIONS, etc... too much data being passed around manage/groups/import/etc.
 	  
 	  (feature) Add ability to view emails in queue (from mailing status)
-
-	  (feature) add personalization to messages
-	  	+ personalization algorithm ->
-	  		[queue is grabbed per page] (TODO defaults to 100, make easier to set @ file header)
-	  			+ adjust queue to also return subscriberID, stored in $queue variable w/ email
-	  		[body is loaded into session] 
-	  			+ if body contains personalization, ENABLE personalization
-	  				+ get position of personalizations & assosiated demographic_id & "default" into PERSONALIZATIONS array
-	  				
-	  				
-	  		[mail leavs throttler to send]
-	  			+ IF _SESSION[PERSONALIZATION] PERSONALIZATION
-	  					+ personalizeBody(subId)
-	  			SEND MAIL
-	  		
-	  		[personalizeBody]	
-	  		personalizeBody(subId)
-	  			IF subId !IN SESSION_BUFFER {
-	  				add to buffer(queue,email)	
-	  			}
-	  			
-	  			alter mailer body... original stored in session, posistions are cached
-	  			
-	  		
-	  		[ADD TO BUFFER]
-	  			+ GRAB FIRST 15 (set in header) sub IDs from subscriber_values -- make sure to include subscriber ID passed
-	  			ADD VALUES to SESSION as ARRAY [subID] [demoID => value || default]
-	  			
-	  			
-	  			}
-	  				
-	  //(feature) add mailing history
 	  (feature) add message templating
 	  (feature) Add Date + Numeric types  [[[{html_select_date}]]]
-	  
-	  (feature) Enhanced subscriber management
 	  (feature) Display flagged subscribers...
-	  
 	  
 	
 	MEDIUM TERM:
@@ -123,14 +57,18 @@ Add validation of subscriber field name (via AJAX?) with personalization -- chec
 			+ Enforce non duplicate subscribers on the DB level!
 	  (API) Seperate lang files for "admin" & "user" directories --> total of 3: user, admin, install ??
 	  
+	  (API) REGEX group filtering
 	  
 	  (API) Use smartyvalidator + custom validation rules for subscription/subscriber update forms!
 	     + get rid of isEmail()?
+	     
+	  (API) Merge current/history mailing table.
 			
-	  (feature) add ability to send "comments" to list administrator upon successfull subscription
-	  (feature) Add search capability to subscriber management
 	  (feature) Add OR to group filtering
 	  (feature) Enhanced subscriber import
+	  (feature) Add 'comment' type to subscriber field which outputs a text area configured to certain # of chars & whose styling is handled via theme template
+	  
+	  (security) Implement Passwords on user information (login.php). Include customizable question/answer pair.
 	
 	  
 	LONG TERM:
@@ -153,31 +91,14 @@ Add validation of subscriber field name (via AJAX?) with personalization -- chec
 
 		 -----
 	  
-	  UNCAT
+	  UNCAT:
+	
+	  With template, add predefined support. E.G. ++ADD UNSUBSCRIBE LINK++ SEE; http://www.iceburg.net/pommo/community/viewtopic.php?id=108
 	  
-	  when we want to set up a mailing in ISO-8859-15 encoding, this mailing is sent as ISO-8859-1 ...
-	I got the reason : the xxx_mailing_current table is created with a column charset varchar(10) and ISO-8859-15 is 11 char's long...
-	
-	For me it's now corrected with this SQL line :
-	- alter table pommo_mailing_current change charset charset varchar(30) not null;
-	
-	-> if it was this, it was a nasty problem :)
-	
-	  REGEX group filtering
-	  Admin notification on subscriber changes/unsubscribes/additions/etc.
-	 
-	 
-	 
-	 ----- 
-	  P.S. : I also get another issue - when users type any special character (umlaut, accent, etc.) in the name field (I added it to the form), the name appears buggy in the admin interface. It is correct in DB but it's a bit annoying because all colums are scrambled in the interface. Any idea???
+	  4. do not forget to put a blank index in all the directories that do not have an index of their own
 	  
-	  corinna: Somtimes i get pages with little "?" for umlaut also.
-	  corinna: when i switch to some ISO 8859 or so then it works
-	  corinna: maybe it is a firefox browser problem or has to do with this (?) ->
-	  corinna: http://www.w3.org/International/O-HTTP-charset
-	  			-> For PHP, use the header() function before generating any content, e.g.: header("Content-type: text/html; charset=utf-8");
-	 
-	 
+	  Personally I would also like to see a "chain" in place for the unsubscribe, eg. it calls the unsuscribe as it does now but then continies onto another php file, by default empty. But this would allow users (admins) to implement any further processing that they wanted to do, i would imagine that the persons email address should be "posted" to this php chainer. This could make the integration of this to any other installation of anything else, eg site registration removal, so much easier.
+	  
 
 [CORINNA]
 
@@ -204,8 +125,6 @@ Add validation of subscriber field name (via AJAX?) with personalization -- chec
 				-> You requested this in the poMMo forum if the execution times will be longer..
 				With little data it is no problem and when one has a lot of data he can always 
 				make a index on them (Indices).
-
-
 
 
 	SHORT TERM
