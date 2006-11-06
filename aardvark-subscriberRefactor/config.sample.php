e6f741c4fff51c21639f22afe17574a553eab2df
Brice Burgess <490144+briceburg@users.noreply.github.com>
2006-11-06 22:10:17 +0000

    Updated config files, provided a SIMPLE example.
    NOTE; reading config files is now much more secure (via a safe parser vs. inclusion)

--- aardvark-subscriberRefactor/config.sample.php
+++ aardvark-subscriberRefactor/config.sample.php
@@ -1,121 +1,99 @@
-<?php 
-/**
- * poMMo Configuration File:
- *   This file sets up your database, language, and debugging options.
- *
- *   IMPORTANT: File must be named "config.php" and saved in the
- * 	"root" directory of your poMMo installation (where bootstrap.php is)
- */
+<?php die(); // DO NOT REMOVE THIS LINE! SERIOUS SECURITY RISK IF REMOVED! ?>
+==========================================================
+Welcome to the poMMo Configuration File! From here you setup your database and
+  other preferences.
+  
+IMPORTANT: This file must be named "config.php" and saved in the "root"
+  directory of your poMMo installation (where bootstrap.php is).
+==========================================================
 
-/************************************************************************
- * ::: MySQL Database Information :::
- *   in order to use poMMo, you must have access to a valid MySQL database.
- *   Contact your webhost for details if you are unsure of its details.
-************************************************************************/
+::: MySQL Database Information :::
 
-// * Set your MySQL username
-$db['username'] = 'pommo';
+Set the MySQL database poMMo should use **
 
-// * Set your MySQL password
-$db['password'] = 'pommo';
+	[db_database] = "pommo"
+	
+Set the MySQL username **
 
-// * Set your MySQL hostname ("localhost" if  your MySQL database is running on the webserver)
-$db['hostname'] = 'localhost';
+	[db_username] = "pommo"
 
-// * Set the name of the MySQL database used by poMMo
-$db['database'] = 'pommo'; 
+Set the MySQL password **
 
-// * Set the table prefix  (change if you intend to have multiple poMMos running from the same database)
-$db['prefix'] = 'pommo_';
+	[db_password] = "pommo"
+	
+Set the MySQL hostname (generally "localhost") **
 
+	[db_hostname] = "localhost"
+	
+Set the table prefix 
+  (change if you intend to have multiple poMMos running from the same database)
+  
+	[db_prefix] = pommo_
+	
+ 	
+::: Language Information :::
+Set this to your desired locale. Current languages available are;
+ bg - Bulgarian					es - Spanish
+ br - Brazilian Portugese	fr - French
+ da - Danish						it - Italian
+ de - German					nl - Dutch
+ en - English						ro - Romanian
 
-/************************************************************************
- * ::: Language Information :::
- *   Set this to your desired locale  -- this is a work in progress
- * 
- *	bg - Bulgarian					es - Spanish
- *	br - Brazilian Portugese		fr - French
- *	da - Danish						it - Italian
- *	de - German						nl - Dutch
- *	en - English						ro - Romanian
-************************************************************************/
-$lang = 'en';
+	[lang] = en
+	
 
+::: Optional Configuration :::
+==========================================================
+Below Options are intended for debugging and overriding automatic configuration
+==========================================================
 
-/******************[ OPTIONAL CONFIGURATION ]*******************
- * (Below options intended for debugging and overriding 
- * automatic configuration)
-*/
+Set debug mode. Enable (on) or disable(off). Debug mode is useful for providing
+  information to developers
+  
+	[debug] = off
 
-/************************************************************************
- * ::: Debugging Information :::
- *   Only modify these values if you'd like to provide information
- *   to the developers.
-*/
+Set the verbosity level of logging (1: Debugging 2: Informational 3: Important[default])
 
-// enable (on) or disable (off) debug mode. Set this to 'on' to provide debugging information
-//  to the developers. Make sure to set it to 'off' when you are finished collecting information.
-$debug = 'off';
-
-// set the verbosity level of logging.
-//  1: Debugging
-//  2: Informational
-//  3: Important (default)
-$verbosity = 3;
-
-/************************************************************************
- * Uncomment (remove leading "//") and define the following 
- * settings to override default values.
- */
+	[verbosity] = 3
 
 
-/************************************************************************
- * ::: Base URL :::
- * 
- * This is the path to pommo relative to the WEB.
- * For example, if poMMo is http://newsletter.mydomain.com/, the baseURL
- * would be '/'. If poMMo is http://www.mydomain.com/mysite/pommo, the
- * baseURL would be '/mysite/pommo/'
- * 
- * Default: Automatically Detected
- * NOTE: Include trailing slash
- */
-// $baseURL = '/mysite/newsletter';
+::: Overrides :::
+  Uncomment (remove the leading "**") to define the following settings
+  
+Set the Base URL.
+  This is the path to poMMo relative to the WEB. Below are examples with value;
+  (poMMo location)                                                 (baseURL value)
+  http://newsletter.mysite.com/                                /
+  http://www.mysite.com/me/pommo                        /me/pommo/
+  
+  NOTE: Include trailing slash
 
-/************************************************************************
- * ::: Cache Directory :::
- * 
- *   poMMo uses this directory to cache templates. By default, it
- *   is set to the "cache" directory in the poMMo root, and can
- *   safely be left blank or commented out (default).
- * 
- *   Make sure the webserver can write to this directory! poMMo
- *   will NOT WORK without being able to write to this directory.
- * 
- *   If you change its location, it is recommended to set it to a path
- *   outside the web root (for security reasons). 
- *  
- *   DO NOT USE A RELATIVE PATH, USE THE FULL SERVER PATH: e.g.
- *   '/home/b/brice/pommoCache'
- * 
-*/
-// $workDir = '/path/to/pommoCache';
+	** [baseURL] = "/mysite/newsletter/";
 
-/************************************************************************
- * ::: Webserver Hostname :::
- * 
- * This is the hostname of the webserver running poMMo
- * 
- * Default: Automatically Detected
- */
-// $hostname = 'www.mysite.com';
+Set the "working" Cache Directory
+  poMMo uses this directory to cache templates. By default, it
+  is set to the "cache" directory in the poMMo root, and can
+  safely be left blank or commented out (default).
+  
+  Make sure the webserver can write to this directory! poMMo
+  will NOT WORK without being able to write to this directory.
  
- /************************************************************************
- * ::: Webserver Port :::
- * 
- * This is the port number of the webserver running poMMo
- * 
- * Default: Automatically Detected [Usually 80, 8080, or 443]
- */
-// $hostport = 8080;
-?>
+  If you change its location, it is recommended to set it to a path
+  outside the web root (for security reasons). 
+  
+  DO NOT USE A RELATIVE PATH, USE THE FULL SERVER PATH: e.g.
+  '/home/b/brice/pommoCache'
+
+	** [workDir] = "/path/to/pommoCache"
+
+
+Set the webserver hostname
+  Default: Automatically Detected
+ 
+	** [hostname] = www.mysite.com
+	
+
+Set the webserver port
+  Default: Automatically Detected [Usually 80, 443, or 8080]	
+	
+	** [hostport] = 8080
