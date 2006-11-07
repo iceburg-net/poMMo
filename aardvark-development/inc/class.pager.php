da2b218c3b5e1c2641b5aebac36e367cd5daa29b
Mountain/\Ash <27637+mountainash@users.noreply.github.com>
2006-11-07 10:17:56 +0000

    URL encoded links for valid output. added classes for style hooks. changed quoteing for easier reading

--- aardvark-development/inc/class.pager.php
+++ aardvark-development/inc/class.pager.php
@@ -48,41 +48,42 @@ class Pager {
 	 * Returns a list of pages in the format of "´ < [pages] > ª" 
 	 ***********************************************************************************/
 	function pageList($curpage, $pages) {
-		$page_list = '<div class="page_list">';
+		$page_list = '';
+		if ($pages > 1) { // no reason to show if you can't click between more than one page
+			$page_list = '<div class="page_list">'."\n";
 
-		/* Print the first and previous page links if necessary */
-		if (($curpage != 1) && ($curpage)) {
-			//$page_list .= "  <a href=\"".$_SERVER['PHP_SELF']."?page=1\" title=\"First Page\">´</a> ";
-			$page_list .= "  <a href=\"".$_SERVER['PHP_SELF']."?page=1\" title=\"First Page\">&#171;</a> ";
-		}
-
-		if (($curpage -1) > 0) {
-			//$page_list .= "<a href=\"".$_SERVER['PHP_SELF']."?page=". ($curpage -1)."\" title=\"Previous Page\"><</a> ";
-			$page_list .= "<a href=\"".$_SERVER['PHP_SELF']."?page=". ($curpage -1)."\" title=\"Previous Page\">&lt;</a> ";
-		}
-
-		/* Print the numeric page list; make the current page unlinked and bold */
-		for ($i = 1; $i <= $pages; $i ++) {
-			if ($i == $curpage) {
-				$page_list .= "<strong>".$i."</strong>";
-			} else {
-				$page_list .= "<a href=\"".$_SERVER['PHP_SELF']."?page=".$i.$this->appendUrl."\" title=\"Page ".$i."\">".$i."</a>";
+			/* Print the first and previous page links if necessary */
+			if (($curpage != 1) && ($curpage)) {
+				//$page_list .= " <a href=\"".$_SERVER['PHP_SELF']."?page=1\"".htmlentities($this->appendUrl)."title=\"First Page\">´</a> ";
+				$page_list .= '<a href="'.$_SERVER['PHP_SELF'].'?page=1'.htmlentities($this->appendUrl).'" class="first" title="First Page">&#171;</a>'."\n";
 			}
-			$page_list .= " ";
-		}
 
-		/* Print the Next and Last page links if necessary */
-		if (($curpage +1) <= $pages) {
-			//$page_list .= "<a href=\"".$_SERVER['PHP_SELF']."?page=". ($curpage +1).$this->appendUrl."\" title=\"Next Page\">></a> ";
-			$page_list .= "<a href=\"".$_SERVER['PHP_SELF']."?page=". ($curpage +1).$this->appendUrl."\" title=\"Next Page\">&gt;</a> ";
-		}
+			if (($curpage -1) > 0) {
+				//$page_list .= "<a href=\"".$_SERVER['PHP_SELF']."?page=". ($curpage -1)."\" title=\"Previous Page\"><</a> ";
+				$page_list .= '<a href="'.$_SERVER['PHP_SELF'].'?page='.($curpage -1).htmlentities($this->appendUrl).'" class="prev" title="Previous Page">&lt;</a>'."\n";
+			}
 
-		if (($curpage != $pages) && ($pages != 0)) {
-			//$page_list .= "<a href=\"".$_SERVER['PHP_SELF']."?page=".$pages.$this->appendUrl."\" title=\"Last Page\">ª</a> ";
-			$page_list .= "<a href=\"".$_SERVER['PHP_SELF']."?page=".$pages.$this->appendUrl."\" title=\"Last Page\">&#187;</a> ";
-		}
-			$page_list .= "</div>\n";
+			/* Print the numeric page list; make the current page unlinked and bold */
+			for ($i = 1; $i <= $pages; $i ++) {
+				if ($i == $curpage) {
+					$page_list .= '<strong>'.$i.'</strong>'."\n";
+				} else {
+					$page_list .= '<a href="'.$_SERVER['PHP_SELF'].'?page='.$i.htmlentities($this->appendUrl).'" title="Page '.$i.'">'.$i.'</a>'."\n";
+				}
+			}
 
+			/* Print the Next and Last page links if necessary */
+			if (($curpage +1) <= $pages) {
+				//$page_list .= "<a href=\"".$_SERVER['PHP_SELF']."?page=". ($curpage +1).htmlentities($this->appendUrl)."\" title=\"Next Page\">></a> ";
+				$page_list .= '<a href="'.$_SERVER['PHP_SELF'].'?page='.($curpage +1).htmlentities($this->appendUrl).'" class="next" title="Next Page">&gt;</a>'."\n";
+			}
+
+			if (($curpage != $pages) && ($pages != 0)) {
+				//$page_list .= "<a href=\"".$_SERVER['PHP_SELF']."?page=".$pages.htmlentities($this->appendUrl)."\" title=\"Last Page\">ª</a> ";
+				$page_list .= '<a href="'.$_SERVER['PHP_SELF'].'?page='.$pages.htmlentities($this->appendUrl).'" class="last" title="Last Page">&#187;</a>'."\n";
+			}
+				$page_list .= '</div>'."\n";
+		}
 		return $page_list;
 	}
 	/*********************************************************************************** 
