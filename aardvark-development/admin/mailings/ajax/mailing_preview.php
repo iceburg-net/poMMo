0c9a9f2fe7b0e7f0562c663a93ab6614b34d3543
Brice Burgess <490144+briceburg@users.noreply.github.com>
2006-12-08 10:29:00 +0000

    Added HTML Message Body Preview

---
+++ aardvark-development/admin/mailings/ajax/mailing_preview.php
@@ -0,0 +1,28 @@
+<?php
+/** [BEGIN HEADER] **
+ * COPYRIGHT: (c) 2005 Brice Burgess / All Rights Reserved    
+ * LICENSE: http://www.gnu.org/copyleft.html GNU/GPL 
+ * AUTHOR: Brice Burgess <bhb@iceburg.net>
+ * SOURCE: http://pommo.sourceforge.net/
+ *
+ *  :: RESTRICTIONS ::
+ *  1. This header must accompany all portions of code contained within.
+ *  2. You must notify the above author of modifications to contents within.
+ * 
+ ** [END HEADER]**/
+ 
+ /**********************************
+	INITIALIZATION METHODS
+ *********************************/
+require ('../../../bootstrap.php');
+Pommo::requireOnce($pommo->_baseDir.'inc/helpers/fields.php');
+
+$pommo->init(array('noDebug' => TRUE, 'keep' => TRUE));
+$logger = & $pommo->_logger;
+$dbo = & $pommo->_dbo;
+
+$input = $pommo->get('mailingData');
+
+die($input['body']);
+
+?>
