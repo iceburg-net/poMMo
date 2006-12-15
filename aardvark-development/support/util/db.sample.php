d65513a74a8c39d22c49ec95b9e99cf7a66684fc
Brice Burgess <490144+briceburg@users.noreply.github.com>
2006-12-15 15:52:13 +0000

    (SUPPORT)   Added Loading of Sample Data   Added clearing of subscribers   Added clearing of database
    Fixed importing of dates
    Fixed importing of non-active fields
    
    Fixed validation of non required fields
    Fixed removal of empty option > bad field in fields_edit.php
    
    Fixed group filtering by numeric/date greater than || less than types

---
+++ aardvark-development/support/util/db.sample.php
@@ -0,0 +1,50 @@
+<?php
+/** [BEGIN HEADER] **
+ * COPYRIGHT: (c) 2005 Brice Burgess / All Rights Reserved    
+ * LICENSE: http://www.gnu.org/copyleft.html GNU/GPL 
+ * AUTHOR: Brice Burgess <bhb@iceburg.net>
+ * SOURCE: http://pommo.sourceforge.net/
+ *
+ *  :: RESTRICTIONS ::
+ *  1. This header must accompany all portions of code contained within.
+ *  2. You must notify the above author of modifications to contents within.
+ * 
+ ** [END HEADER]**/
+ 
+// Clears the entire database, resets auto increment values
+ 
+/**********************************
+	INITIALIZATION METHODS
+ *********************************/
+define('_poMMo_support', TRUE);
+
+require ('../../bootstrap.php');
+Pommo::requireOnce($pommo->_baseDir.'install/helper.install.php'); // MUST HAVE THIS HERE!
+
+if (bmIsInstalled())
+	$pommo->init();
+else
+	$pommo->init(array('authLevel' => 0));
+	
+$dbo =& $pommo->_dbo;
+
+// reset DB
+
+foreach($dbo->table as $id => $table) {
+	if($id == 'config' || $id == 'updates')
+		continue;
+		
+	$query = "DELETE FROM ".$table;
+	if(!$dbo->query($query))
+		die('ERROR deleting '.$id); 
+		
+	$query = "ALTER TABLE ".$table." AUTO_INCREMENT = 1";
+	if(!$dbo->query($query))
+		die('ERROR setting AUTO_INCREMENT on '.$id); 
+}
+
+$file = $pommo->_baseDir."install/sql.sample.php";
+if(!parse_mysql_dump(false,$file))
+	die('Could not load sample data. Database Reset.');
+
+die('Database Reset. Sample Data Loaded.');
