9da2bb50a80f3328e591e1c57f81674ec0223754
briceb <briceb@00ef1af9-9410-0410-96a5-8f45c99aad9b>
2007-01-17 07:52:31 +0000

    Updated file header

--- aardvark-development/support/tests/file.clearWork.php
+++ aardvark-development/support/tests/file.clearWork.php
@@ -1,13 +1,19 @@
 <?php
 /** [BEGIN HEADER] **
  * COPYRIGHT: (c) 2005 Brice Burgess / All Rights Reserved    
- * LICENSE: http://www.gnu.org/copyleft.html GNU/GPL 
+ * LICENSE: http://www.gnu.org/licenses/gpl.html GNU/GPL 
  * AUTHOR: Brice Burgess <bhb@iceburg.net>
  * SOURCE: http://pommo.sourceforge.net/
  *
  *  :: RESTRICTIONS ::
  *  1. This header must accompany all portions of code contained within.
- *  2. You must notify the above author of modifications to contents within.
+ *  2. Notify the above author of modifications to contents within.
+ * 
+ *  WHY? Because this is a community project -- purposely released under the GPL.
+ *    We'd love to have the possiblity to include your derivative works! 
+ *    We'd love to coordinate around your development efforts!
+ *    We'd love to assist you with your changes!
+ *    DON'T BE A STRANGER!
  * 
  ** [END HEADER]**/
  
@@ -31,7 +37,7 @@ function delDir($dirName) {
 				if (is_dir($dirName . '/' . $file)) {
 					delDir($dirName . '/' . $file);
 				} else {
-					@ unlink($dirName . '/' . $file) or die('File ' . $dirName . '/' . $file . ' couldn\'t be deleted!');
+					unlink($dirName . '/' . $file) or die('File ' . $dirName . '/' . $file . ' couldn\'t be deleted!');
 				}
 			}
 		}
