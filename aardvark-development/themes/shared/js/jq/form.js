da1ced81f4b4012136be9bcb4af03d6a89fc1b22
Brice Burgess <490144+briceburg@users.noreply.github.com>
2007-02-17 09:02:26 +0000

    Javascript additions;
    Added jQuery 1.1 distribution -- will replace 1.0.3 after code audit.
    
    Added jqModal plugin with poMMo dialog graphics adj Drag & Drop support (jqDnR)
    
    Added accordion plugin
    
    Updated forms plugin
    
    Updated interface plugin

--- aardvark-development/themes/shared/js/jq/form.js
+++ aardvark-development/themes/shared/js/jq/form.js
@@ -1,48 +1,133 @@
 /*
  * jQuery form plugin
- * @requires jQuery v1.0.2
+ * @requires jQuery v1.0.3
  *
  * Dual licensed under the MIT and GPL licenses:
  *   http://www.opensource.org/licenses/mit-license.php
  *   http://www.gnu.org/licenses/gpl.html
  *
- * Revision: $Id$
+ * Revision: 1357 @ jQ SVN
  */
+
+jQuery.fn.ajaxSubmit = function(options) {
+    if (typeof options == 'function')
+        options = { success: options };
+
+    options = jQuery.extend({
+        url:    this.attr('action') || '',
+        method: this.attr('method') || 'GET'
+    }, options || {});
+
+    // remap deprecated options (temporarily)
+    options.success = options.success || options.after;
+    options.beforeSubmit = options.beforeSubmit || options.before;
+    options.type = options.type || options.method;
+
+    var a = this.formToArray(options.semantic);
+
+    // give pre-submit callback an opportunity to abort the submit
+    if (options.beforeSubmit && options.beforeSubmit(a, this, options) === false) return this;
+
+    var q = jQuery.param(a);
+
+    if (options.type.toUpperCase() == 'GET') {
+        // if url already has a '?' then append args after '&'
+        options.url += (options.url.indexOf('?') >= 0 ? '&' : '?') + q;
+        options.data = null;  // data is null for 'get'
+    }
+    else
+        options.data = q; // data is the query string for 'post'
+
+    var $form = this, callbacks = [];
+    if (options.resetForm) callbacks.push(function() { $form.resetForm(); });
+    if (options.clearForm) callbacks.push(function() { $form.clearForm(); });
+
+    // perform a load on the target only if dataType is not provided
+    if (!options.dataType && options.target) {
+        var oldSuccess = options.success || function(){};
+        callbacks.push(function(data, status) {
+            jQuery(options.target).attr("innerHTML", data).evalScripts().each(oldSuccess, [data, status]);
+        });
+    }
+    else if (options.success)
+        callbacks.push(options.success);
+
+    options.success = function(data, status) {
+        for (var i=0, max=callbacks.length; i < max; i++)
+            callbacks[i](data, status);
+    };
+
+    jQuery.ajax(options);
+    return this;
+};
+
+jQuery.fn.ajaxForm = function(options) {
+    return this.each(function() {
+        jQuery("input:submit,input:image,button:submit", this).click(function(ev) {
+            var $form = this.form;
+            $form.clk = this;
+            if (this.type == 'image') {
+                if (ev.offsetX != undefined) {
+                    $form.clk_x = ev.offsetX;
+                    $form.clk_y = ev.offsetY;
+                } else if (typeof jQuery.fn.offset == 'function') { // try to use dimensions plugin
+                    var offset = jQuery(this).offset();
+                    $form.clk_x = ev.pageX - offset.left;
+                    $form.clk_y = ev.pageY - offset.top;
+                } else {
+                    $form.clk_x = ev.pageX - this.offsetLeft;
+                    $form.clk_y = ev.pageY - this.offsetTop;
+                }
+            }
+            // clear form vars
+            setTimeout(function() {
+                $form.clk = $form.clk_x = $form.clk_y = null;
+                }, 10);
+        })
+    }).submit(function(e) {
+        jQuery(this).ajaxSubmit(options);
+        return false;
+    });
+};
 
 jQuery.fn.formToArray = function(semantic) {
     var a = [];
-    var q = semantic ? ':input' : 'input,textarea,select,button';
+    if (this.length == 0) return a;
 
-    jQuery(q, this).each(function() {
-        var n = this.name;
-        var t = this.type;
-        var tag = this.tagName.toLowerCase();
+    var form = this[0];
+    var els = semantic ? form.getElementsByTagName('*') : form.elements;
+    if (!els) return a;
+    for(var i=0, max=els.length; i < max; i++) {
+        var el = els[i];
+        var n = el.name;
+        if (!n) continue;
 
-        if ( !n || this.disabled || t == 'reset' ||
-            (t == 'checkbox' || t == 'radio') && !this.checked ||
-            (t == 'submit' || t == 'image' || t == 'button') && this.form && this.form.clk != this ||
-            tag == 'select' && this.selectedIndex == -1)
-            return;
-
-        if (t == 'image' && this.form.clk_x != undefined)
-            return a.push(
-                {name: n+'_x', value: this.form.clk_x},
-                {name: n+'_y', value: this.form.clk_y}
-            );
-
-        if (tag == 'select') {
-            // pass select element off to fieldValue to reuse the IE logic
-            var val = jQuery.fieldValue(this, false); // pass false to optimize fieldValue
-            if (t == 'select-multiple') {
-                for (var i=0; i < val.length; i++)
-                    a.push({name: n, value: val[i]});
-            }
-            else
-                a.push({name: n, value: val});
+        if (semantic && form.clk && el.type == "image") {
+            // handle image inputs on the fly when semantic == true
+            if(!el.disabled && form.clk == el)
+                a.push({name: n+'.x', value: form.clk_x}, {name: n+'.y', value: form.clk_y});
+            continue;
+        }
+        var v = jQuery.fieldValue(el, true);
+        if (v === null) continue;
+        if (v.constructor == Array) {
+            for(var j=0, jmax=v.length; j < jmax; j++)
+                a.push({name: n, value: v[j]});
         }
         else
-            a.push({name: n, value: this.value});
-    });
+            a.push({name: n, value: v});
+    }
+
+    if (!semantic && form.clk) {
+        // input type=='image' are not found in elements array! handle them here
+        var inputs = form.getElementsByTagName("input");
+        for(var i=0, max=inputs.length; i < max; i++) {
+            var input = inputs[i];
+            var n = input.name;
+            if(n && !input.disabled && input.type == "image" && form.clk == input)
+                a.push({name: n+'.x', value: form.clk_x}, {name: n+'.y', value: form.clk_y});
+        }
+    }
     return a;
 };
 
@@ -52,18 +137,18 @@ jQuery.fn.formSerialize = function(semantic) {
     return jQuery.param(this.formToArray(semantic));
 };
 
-
 jQuery.fn.fieldSerialize = function(successful) {
     var a = [];
     this.each(function() {
-        if (!this.name) return;
-        var val = jQuery.fieldValue(this, successful);
-        if (val && val.constructor == Array) {
-            for (var i=0; i < val.length; i++)
-                a.push({name: this.name, value: val[i]});
+        var n = this.name;
+        if (!n) return;
+        var v = jQuery.fieldValue(this, successful);
+        if (v && v.constructor == Array) {
+            for (var i=0,max=v.length; i < max; i++)
+                a.push({name: n, value: v[i]});
         }
-        else if (val !== null && typeof val != 'undefined')
-            a.push({name: this.name, value: val});
+        else if (v !== null && typeof v != 'undefined')
+            a.push({name: this.name, value: v});
     });
     //hand off to jQuery.param for proper encoding
     return jQuery.param(a);
@@ -71,54 +156,79 @@ jQuery.fn.fieldSerialize = function(successful) {
 
 
 jQuery.fn.fieldValue = function(successful) {
-    var cbVal = [], cbName = null;
+    var cbVal, cbName;
 
     // loop until we find a value
-    for (var i = 0; i < this.length; i++) {
+    for (var i=0, max=this.length; i < max; i++) {
         var el = this[i];
-        if (el.type == 'checkbox') {
-            if (!cbName) cbName = el.name || 'unnamed';
-            if (cbName != el.name) // return if we hit a checkbox with a different name
-                return cbVal;
-            var val = jQuery.fieldValue(el, successful);
-            if (val !== null && typeof val != 'undefined') 
-                cbVal.push(val);
-        }
-        else {
-            var val = jQuery.fieldValue(el, successful);
-            if (val !== null && typeof val != 'undefined') 
-                return val;
-        }
+        var v = jQuery.fieldValue(el, successful);
+        if (v === null || typeof v == 'undefined' || (v.constructor == Array && !v.length))
+            continue;
+
+        // for checkboxes, consider multiple elements, for everything else just return first valid value
+        if (el.type != 'checkbox') return v;
+
+        cbName = cbName || el.name;
+        if (cbName != el.name) // return if we hit a checkbox with a different name
+            return cbVal;
+        cbVal = cbVal || [];
+        cbVal.push(v);
     }
     return cbVal;
 };
 
-
 jQuery.fieldValue = function(el, successful) {
-    var n = el.name;
-    var t = el.type;
-    var tag = el.tagName.toLowerCase();
+    var n = el.name, t = el.type, tag = el.tagName.toLowerCase();
     if (typeof successful == 'undefined') successful = true;
 
     if (successful && ( !n || el.disabled || t == 'reset' ||
         (t == 'checkbox' || t == 'radio') && !el.checked ||
-        (t == 'submit' || t == 'image' || t == 'button') && el.form && el.form.clk != el ||
+        (t == 'submit' || t == 'image') && el.form && el.form.clk != el ||
         tag == 'select' && el.selectedIndex == -1))
             return null;
-    
+
     if (tag == 'select') {
-        var a = [];
-        for(var i=0; i < el.options.length; i++) {
-            var op = el.options[i];
+        var index = el.selectedIndex;
+        if (index < 0) return null;
+        var a = [], ops = el.options;
+        var one = (t == 'select-one');
+        var max = (one ? index+1 : ops.length);
+        for(var i=(one ? index : 0); i < max; i++) {
+            var op = ops[i];
             if (op.selected) {
                 // extra pain for IE...
                 var v = jQuery.browser.msie && !(op.attributes['value'].specified) ? op.text : op.value;
-                if (t == 'select-one')
-                    return v;
+                if (one) return v;
                 a.push(v);
             }
         }
         return a;
     }
     return el.value;
+};
+
+jQuery.fn.clearForm = function() {
+    return this.each(function() {
+        jQuery('input,select,textarea', this).clearFields();
+    });
+};
+
+jQuery.fn.clearFields = jQuery.fn.clearInputs = function() {
+    return this.each(function() {
+        var t = this.type, tag = this.tagName.toLowerCase();
+        if (t == 'text' || t == 'password' || tag == 'textarea')
+            this.value = '';
+        else if (t == 'checkbox' || t == 'radio')
+            this.checked = false;
+        else if (tag == 'select')
+            this.selectedIndex = -1;
+    });
+};
+
+jQuery.fn.resetForm = function() {
+    return this.each(function() {
+
+        if (typeof this.reset == 'function' || (typeof this.reset == 'object' && !this.reset.nodeType))
+            this.reset();
+    });
 };
