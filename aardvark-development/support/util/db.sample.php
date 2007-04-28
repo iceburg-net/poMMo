da7f4307e4a76aa2004a06fa7cdee0adfaf4b052
briceb <briceb@00ef1af9-9410-0410-96a5-8f45c99aad9b>
2007-04-28 20:32:44 +0000

    Corrected loading of sample data (support library)

--- aardvark-development/support/util/db.sample.php
+++ aardvark-development/support/util/db.sample.php
@@ -33,7 +33,7 @@ $dbo =& $pommo->_dbo;
 // reset DB
 
 foreach($dbo->table as $id => $table) {
-	if($id == 'config' || $id == 'updates' || $id == 'group_criteria')
+	if($id == 'config' || $id == 'updates' || $id == 'group_criteria' || $id == 'subscriber_update')
 		continue;
 		
 	$query = "DELETE FROM ".$table;
