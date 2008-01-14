494a02d7efa3f2c5fbbc850c66491cb774a7d311
Brice Burgess <490144+briceburg@users.noreply.github.com>
2008-01-14 00:42:59 +0000

    PR16rc3 Release

--- aardvark-development/inc/classes/pommo.php
+++ aardvark-development/inc/classes/pommo.php
@@ -23,7 +23,7 @@
 */
 
 class Pommo {
-	var $_revision = 38; // poMMo's current "file" revision #
+	var $_revision = 39; // poMMo's current "file" revision #
 
 	var $_dbo; // holds the database object
 	var $_logger; // holds the logger (messaging) object
